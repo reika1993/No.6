5ee8816ae844fddd49fb56792ebca3a8aa66fd25
reika1993 <miii1120.rei@gmail.com>
2023-12-10 17:18:15 +0900

    HelloWorldの表記変更

--- src/main/java/com/no6/no6/HelloController.java
+++ src/main/java/com/no6/no6/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "HelloWorld!";
+        return "HelloWorld!!";
     }
 
 }
